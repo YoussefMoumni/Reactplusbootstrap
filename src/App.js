add188c70cf0e7cdcf4e9e62f1c10874a88ba702
Youssef Moumni <youssef.moumni1@ibm.com>
2019-10-18 17:04:37 +0100

    fisrt commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => (
+    <>
+    <h1>Hello </h1>
+    <p> Minimal React conf. </p> 
+    </>
+)
+ReactDOM.render(<App />, document.getElementById("root"))
